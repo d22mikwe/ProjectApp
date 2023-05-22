62e67a3ded3373b4146770968619068d5ece6836
Micke <micke.welander@live.se>
2023-05-22 16:47:39 +0200

    Due to several prioblems with android studio ive had to recreate the project. Therefore im commiting the same code in the span of a short time

---
+++ app/src/main/java/com/example/projectapp/RecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.projectapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+
+    private List<DogItem> dogItemList;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+
+    public RecyclerViewAdapter(Context context, List<DogItem> items, OnClickListener onClickListener) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.dogItemList = items;
+        this.onClickListener = onClickListener;
+    }
+
+
+    @Override
+    @NonNull
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.item_list, parent, false));
+    }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.title.setText(dogItemList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dogItemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(dogItemList.get(getAdapterPosition()));
+        }
+
+
+    }
+    public interface OnClickListener{
+        void onClick(DogItem item);
+    }
+}
